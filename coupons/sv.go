458b503c8cbdb203ea63ff3b878c91ab3dca0640
guou <guoyu2@zetyun.com>
2022-03-11 09:22:10 +0800

    fix error

--- coupons/sv.go
+++ coupons/sv.go
@@ -14,6 +14,7 @@ type Coupons struct {
 	Now          int    `json:"now"`
 	End          int    `json:"end"`
 	Limitprice   int    `json:"limitPrice"`
+	CouponPrice  int    `json:"couponPrice"`
 	Key          string `json:"key"`
 	Remark       string `json:"remark"`
 	Couponid     string `json:"couponId"`
@@ -21,7 +22,7 @@ type Coupons struct {
 
 func (s Coupons) GetCouponsItems(t string) []Coupons {
 	ss := []Coupons{
-		{ValidateType: 2, ValidityTime: 1, Now: 0, End: 1, Limitprice: 10, Key: "1", Remark: "不能叠使用", Couponid: "1"},
+		{ValidateType: 2, ValidityTime: 1, Now: 0, End: 1, Limitprice: 10, CouponPrice: 3, Key: "1", Remark: "不能叠使用", Couponid: "1"},
 	}
 	return ss
 }
